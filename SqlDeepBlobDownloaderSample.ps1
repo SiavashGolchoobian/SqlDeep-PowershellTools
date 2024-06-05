4a306b539ed6cc203eb989d5583cb0e790ba9060
Siavash Golchoobian <siavash.golchoobian@gmail.com>
2024-06-05 15:52:03 +0330

    Update SqlDeepBlobDownloaderSample.ps1

--- SqlDeepBlobDownloaderSample.ps1
+++ SqlDeepBlobDownloaderSample.ps1
@@ -18,3 +18,22 @@ $myFileQueryList.("file1.txt")="SELECT TOP 1 [FileContent] FROM [SqlDeep].[dbo].
 $myFileQueryList.("file2.txt")="SELECT TOP 1 [FileContent] FROM [SqlDeep].[dbo].[ScriptRepositoryGuest] WITH (READPAST) WHERE [IsEnabled]=1 AND [HostChecksum]=[GuestChecksum] AND [FileUniqueName]=N'file2.txt'"
 $myFileQueryList.("file3.txt")="SELECT TOP 1 [FileContent] FROM [SqlDeep].[dbo].[ScriptRepositoryGuest] WITH (READPAST) WHERE [IsEnabled]=1 AND [HostChecksum]=[GuestChecksum] AND [FileUniqueName]=N'file3.txt'"
 $myAnswer=DownloadMultipleFilesFromDB -ConnectionString $myConnectionString -FileQueryList $myFileQueryList -DestinationFolderPath $myDestinationFolderPath
+
+#Sample 3: Download list of files via query
+[bool]$myAnswer
+[string]$myConnectionString="Data Source=DB-MN-DLV01.SQLDEEP.LOCAL\NODE,49149;Initial Catalog=EventLog;Integrated Security=True;TrustServerCertificate=True;Encrypt=True"
+[string]$myDestinationFolderPath="C:\Repo\DatabaseShipping"
+[string]$myCommand="SELECT [myItems].[ItemName],[myItems].[SubscriberItemId] FROM [SqlDeep].[repository].[dbafn_get_subscriber_item_and_dependencies] ('Saipa_SqlDeepDatabaseShipping.ps1',Null,Null) AS myItems WHERE [myItems].[IsEnabled]=1"
+$myFileQueryList=@{}
+try{
+    $myRecord=Invoke-Sqlcmd -ConnectionString $myConnectionString -Query $myCommand -OutputSqlErrors $true -QueryTimeout 0 -OutputAs DataRows -ErrorAction Stop
+    $myRecord
+    if ($null -ne $myRecord) {
+        foreach ($myRow in $myRecord){
+            $myFileQueryList.Add($myRow.ItemName.ToString(),"SELECT TOP 1 [ItemContent] FROM [SqlDeep].[repository].[Subscriber] WITH (READPAST) WHERE [SubscriberItemId]=" + $myRow.SubscriberItemId.ToString())
+        }
+    }
+    $myAnswer=DownloadMultipleFilesFromDB -ConnectionString $myConnectionString -FileQueryList $myFileQueryList -DestinationFolderPath $myDestinationFolderPath
+}Catch{
+    Write-Output(($_.ToString()).ToString())
+}
