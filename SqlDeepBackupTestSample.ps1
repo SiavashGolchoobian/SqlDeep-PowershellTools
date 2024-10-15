24a96f386b25d599984816a215badd919d199e18
FMoniri <FMoniri@Gmail.com>
2024-10-15 08:34:09 +0330

    Update SqlDeepBackupTestSample.ps1

--- SqlDeepBackupTestSample.ps1
+++ SqlDeepBackupTestSample.ps1
@@ -2,16 +2,16 @@ using module .\SqlDeepLogWriter.psm1
 using module .\SqlDeepBackupTest.psm1
 using module .\SqlDeepCommon.psm1
 
-$myLogWriter=New-LogWriter -EventSource ($env:computername) -Module "BackupTest" -LogToConsole -LogToFile -LogFilePath "C:\Temp\BackupTest_{Database}_{Date}.txt" -LogToTable -LogInstanceConnectionString "Data Source=467960T1086967\SQLDEV;Initial Catalog=EventLog;Integrated Security=True;TrustServerCertificate=True;Encrypt=True" -LogTableName "[dbo].[myTestTable]"
+$myLogWriter=New-LogWriter -EventSource ($env:computername) -Module "BackupTest" -LogToConsole -LogToFile -LogFilePath "C:\Temp\BackupTest_{Database}_{Date}.txt" -LogToTable -LogInstanceConnectionString "Data Source=MachinName\InstanceName;Initial Catalog=EventLog;Integrated Security=True;TrustServerCertificate=True;Encrypt=True" -LogTableName "[dbo].[myTestTable]"
 $myBackupTest=[BackupTest]::New()
 $myBackupTest.LogWriter=$myLogWriter
 #$myShip=New-DatabaseShipping -SourceInstanceConnectionString "Data Source=LSNR.SQLDEEP.LOCAL\NODE,49149;Initial Catalog=master;Integrated Security=True;TrustServerCertificate=True;Encrypt=True" -DestinationInstanceConnectionString "Data Source=DB-DR-DGV01.SQLDEEP.LOCAL\NODE,49149;Initial Catalog=master;Integrated Security=True;TrustServerCertificate=True;Encrypt=True" -FileRepositoryUncPath "\\db-dr-dgv01\Backups" -DestinationRestoreMode ([DatabaseRecoveryMode]::RESTOREONLY) -LogWrite $myLogWriter -LimitMsdbScanToRecentHours 24 -RestoreFilesToIndividualFolders
 
 #Sample 1:
 [BackupTest]$myDatabaseTest=$null
-#$myDatabaseTest=New-DatabaseTest -SourceInstanceConnectionString "Data Source=DB-C1-DLV16.SQLDEEP.LOCAL\NODE,49149;Initial Catalog=master;Integrated Security=True;TrustServerCertificate=True;Encrypt=True" -DestinationInstanceConnectionString "Data Source=DB-BK-DBV02.SQLDEEP.LOCAL\NODE,49149;Initial Catalog=master;Integrated Security=True;TrustServerCertificate=True;Encrypt=True" -LogWrite $myLogWriter -BackupTestCatalogTableName "myTestTable" # -FileRepositoryUncPath "\\DB-BK-DBV02\U$\Databases\Backup"
-$myDatabaseTest=New-DatabaseTest -SourceInstanceConnectionString "Data Source=DB-C1-DLV16.SQLDEEP.LOCAL\NODE,49149;Initial Catalog=master;Integrated Security=True;TrustServerCertificate=True;Encrypt=True" -DestinationInstanceConnectionString "Data Source=DB-BK-DBV02.SQLDEEP.LOCAL\NODE,49149;Initial Catalog=master;Integrated Security=True;TrustServerCertificate=True;Encrypt=True" -LogWrite $myLogWriter -BackupTestCatalogTableName "myTestTable" # -FileRepositoryUncPath "\\DB-BK-DBV02\U$\Databases\Backup"
+$myDatabaseTest=New-DatabaseTest -SourceInstanceConnectionString "Data Source=MachinName.Domainname\InstanceName,PortNo;Initial Catalog=master;Integrated Security=True;TrustServerCertificate=True;Encrypt=True" -DestinationInstanceConnectionString "Data Source=MachinName.Domainname\InstanceName,PortNo;Initial Catalog=master;Integrated Security=True;TrustServerCertificate=True;Encrypt=True" -LogWrite $myLogWriter -BackupTestCatalogTableName "myTestTable" # -FileRepositoryUncPath "\\DB-BK-DBV02\U$\Databases\Backup"
 
+#.$myDatabaseTest.StartDate= "10/14/2024 14:45:00" 
 $myDatabaseTest.StartDate=(Get-Date).AddDays(-2) 
 $myDatabaseTest.EndDate=Get-Date
 #$myDatabaseTest.TestAllDatabases("sqldeep")
@@ -21,18 +21,18 @@ $myDatabaseTest.TestDatabase("MixI")
 #$myDatabaseTest.RestoreTime or $myDatabaseTest.RestoreTo
 <#
 #Sample 2 :
-$myDatabaseTest=New-DatabaseTest -SourceInstanceConnectionString "Data Source=DB-MN-DLV02.SQLDEEP.LOCAL\NODE,49149;Initial Catalog=master;Integrated Security=True;TrustServerCertificate=True;Encrypt=True" -DestinationInstanceConnectionString "Data Source=DB-BK-DBV02.SQLDEEP.LOCAL\NODE,49149;Initial Catalog=master;Integrated Security=True;TrustServerCertificate=True;Encrypt=True" -LogWrite $myLogWriter -BackupTestCatalogTableName "myTestTable"
+$myDatabaseTest=New-DatabaseTest -SourceInstanceConnectionString "Data Source=MachinName.Domainname\InstanceName,PortNo;Initial Catalog=master;Integrated Security=True;TrustServerCertificate=True;Encrypt=True" -DestinationInstanceConnectionString "Data Source=DB-BK-DBV02.SQLDEEP.LOCAL\NODE,49149;Initial Catalog=master;Integrated Security=True;TrustServerCertificate=True;Encrypt=True" -LogWrite $myLogWriter -BackupTestCatalogTableName "myTestTable"
 
 [string[]]$ExcludedDatabaseList = "sqldeep","DWDiagnostics","SSISDB"
 $myDatabaseTest.TestAllDatabases($ExcludedDatabaseList)
 
-$SourceInstanceConnectionString = "Data Source=DB-C1-DLV16.SQLDEEP.LOCAL\NODE,49149;Initial Catalog=master;Integrated Security=True;TrustServerCertificate=True;Encrypt=True" 
+$SourceInstanceConnectionString = "Data Source=MachinName.Domainname\InstanceName,PortNo;Initial Catalog=master;Integrated Security=True;TrustServerCertificate=True;Encrypt=True" 
 
 #Sample 3 :
-$myDatabaseTest=New-DatabaseTest -SourceInstanceConnectionString "Data Source=DB-MN-DLV02.SQLDEEP.LOCAL\NODE,49149;Initial Catalog=master;Integrated Security=True;TrustServerCertificate=True;Encrypt=True" -DestinationInstanceConnectionString "Data Source=DB-BK-DBV02.SQLDEEP.LOCAL\NODE,49149;Initial Catalog=master;Integrated Security=True;TrustServerCertificate=True;Encrypt=True" -LogWrite $myLogWriter -BackupTestCatalogTableName "myTestTable"
+$myDatabaseTest=New-DatabaseTest -SourceInstanceConnectionString "Data Source=MachinName.Domainname\InstanceName,PortNo;Initial Catalog=master;Integrated Security=True;TrustServerCertificate=True;Encrypt=True" -DestinationInstanceConnectionString "Data Source=DB-BK-DBV02.SQLDEEP.LOCAL\NODE,49149;Initial Catalog=master;Integrated Security=True;TrustServerCertificate=True;Encrypt=True" -LogWrite $myLogWriter -BackupTestCatalogTableName "myTestTable"
 
 [string[]]$ExcludedDatabaseList = "sqldeep","DWDiagnostics","SSISDB"
-[string[]]$ExcludedInstanceList = "DB-C1-DLV11.SQLDEEP.LOCAL\NODE,49149","DB-C1-DLV12.SQLDEEP.LOCAL\NODE,49149","DB-C1-DLV13.SQLDEEP.LOCAL\NODE,49149","DB-C1-DLV14.SQLDEEP.LOCAL\NODE,49149","DB-C1-DLV15.SQLDEEP.LOCAL\NODE,49149","DB-C1-DLV16.SQLDEEP.LOCAL\NODE,49149","DB-SH-DLV01.SQLDEEP.LOCAL\SHAREPOINT,49149"
+[string[]]$ExcludedInstanceList = "MachinName.Domainname\InstanceName,PortNo","MachinName.Domainname\InstanceName,PortNo"
 
 $myDatabaseTest.TestFromRegisterServer($ExcludedInstanceList,$ExcludedDatabaseList)
 
